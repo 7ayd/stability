7539a59842d2c134610816020e6682a31b1c2bb0
7ayd <zalashini3@gatech.edu>
2023-06-20 17:08:37 -0700

    stuff

---
+++ src/Stability.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "{ERC20Burnable.sol}, ERC20.sol}" from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+import "{Ownable.sol}" from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Stability is ERC20Burnable, Ownable {
+    constructor() ERC20("Stability", "USDS"){}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if()
+    }
+
+}
