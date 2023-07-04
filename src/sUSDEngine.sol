23bf75af97fee4cc5a2c101708471c0399dbe712
7ayd <zalashini3@gatech.edu>
2023-07-04 11:52:26 -0700

    Create sUSDEngine.sol

---
+++ src/sUSDEngine.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title sUSDEngine
+ * @author this.7ayd
+ *
+ * This system maintains the properites of the sUSD token.
+ * - Dollar Pegged
+ * - Algorithmically Stable
+ * - Overcollateralized
+ *
+ * @notice this handles all the logic for the Stability Stablecoin Ecosystem.
+ */
+
+contract sUSDEngine {
+    function depositCollateralAndMint() external {}
+
+    function redeemCollateral() external {}
+
+    function burnsUSD() external {}
+
+    function liquidate() external {}
+}
