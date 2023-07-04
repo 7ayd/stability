ea49f53deefceaf818820dd95c6e400ca2b2e8e0
7ayd <zalashini3@gatech.edu>
2023-07-04 11:47:28 -0700

    finished erc20 token

--- src/Stability.sol
+++ src/Stability.sol
@@ -2,20 +2,39 @@
 
 pragma solidity ^0.8.18;
 
-import "{ERC20Burnable.sol, ERC20.sol}" from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-import "{Ownable.sol}" from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Stability is ERC20Burnable, Ownable {
-    constructor() ERC20("Stability", "sUSD"){}
+    error MustBeMoreThanZero();
+    error InsufficientBalance();
+    error ZeroAddress();
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    constructor() ERC20("Stability", "sUSD") {}
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert ZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
     }
 
-    function burn(address from, uint256 amount) public override onlyOwner {
+    function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
-        if()
+        if (_amount <= 0) {
+            revert MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert InsufficientBalance();
+        }
+        super.burn(_amount);
     }
-
 }
