16da6006a63b7e21534c103d7b8a8e67c05ed9a8
7ayd <zalashini3@gatech.edu>
2023-07-05 19:54:23 -0700

    Update sUSDEngine.sol

--- src/sUSDEngine.sol
+++ src/sUSDEngine.sol
@@ -15,11 +15,52 @@ pragma solidity ^0.8.18;
  */
 
 contract sUSDEngine {
+    //////// ERRORS ////////
+    error MustBeMoreThanZero();
+
+    //////// STATE VARIABLES ////////
+    mapping(address token => address priceFeed) public collateralAllowed; // mapping of collateral to price feed
+
+    ///////// MODIFIERS /////////
+    modifier moreThanZero(uint256 _amount) {
+        if (_amount <= 0) {
+            revert MustBeMoreThanZero();
+        }
+        _;
+    }
+
+    modifier collateralAllowed(address _collateral) {
+        require(
+            collateralAllowed[_collateral] != address(0),
+            "Collateral not allowed"
+        );
+        _;
+    }
+
+    ///////// CONSTRUCTOR /////////
+    constructor() {}
+
+    ///////// Functions /////////
     function depositCollateralAndMint() external {}
 
+    /*
+     * @param _collateral The address of the collateral to deposit
+     * @param _amount The amount of collateral to deposit
+     */
+    function deposit(
+        address _collateral,
+        uint256 _amount
+    ) external moreThanZero(_amount) {}
+
     function redeemCollateral() external {}
+
+    function redeemCollateralForStable() external {}
+
+    function mintsUDC() external {}
 
     function burnsUSD() external {}
 
     function liquidate() external {}
+
+    function getHealthFactor() external view {}
 }
